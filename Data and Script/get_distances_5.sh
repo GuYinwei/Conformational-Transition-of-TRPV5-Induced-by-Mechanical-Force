a6ed24be224545414768ffb1b1e691542e638445
GuYinwei <132122486+GuYinwei@users.noreply.github.com>
2023-05-14 22:49:53 +0800

    Add files via upload

---
+++ Data and Script/get_distances_5.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#################################################
+# get_distances.sh
+#
+#   Script iteratively calls gmx distance and
+#   assembles a series of COM distance values
+#   indexed by frame number, for use in
+#   preparing umbrella sampling windows.
+#
+# Written by: Justin A. Lemkul, Ph.D.
+#    Contact: jalemkul@vt.edu
+#
+#################################################
+# compute distances
+for (( i=0; i<601; i++ ))
+do
+    gmx distance -s pull_umbrella_3.tpr -f conf_umbrella_3${i}.gro -n index_pull_umbrella_4.ndx -select 'com of group "P_13" plus com of group "P_14"' -oall dist${i}.xvg 
+done
+
+# compile summary
+touch summary_distances_3_5.dat
+for (( i=0; i<601; i++ ))
+do
+    d=`tail -n 1 dist${i}.xvg | awk '{print $2}'`
+    echo "${i} ${d}" >> summary_distances_3_5.dat
+    rm dist${i}.xvg
+done
+
+exit;
---
+++ Data and Script/get_distances_6.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#################################################
+# get_distances.sh
+#
+#   Script iteratively calls gmx distance and
+#   assembles a series of COM distance values
+#   indexed by frame number, for use in
+#   preparing umbrella sampling windows.
+#
+# Written by: Justin A. Lemkul, Ph.D.
+#    Contact: jalemkul@vt.edu
+#
+#################################################
+# compute distances
+for (( i=0; i<601; i++ ))
+do
+    gmx distance -s pull_umbrella_3.tpr -f conf_umbrella_3${i}.gro -n index_pull_umbrella_4.ndx -select 'com of group "P_15" plus com of group "P_16"' -oall dist${i}.xvg 
+done
+
+# compile summary
+touch summary_distances_3_6.dat
+for (( i=0; i<601; i++ ))
+do
+    d=`tail -n 1 dist${i}.xvg | awk '{print $2}'`
+    echo "${i} ${d}" >> summary_distances_3_6.dat
+    rm dist${i}.xvg
+done
+
+exit;
